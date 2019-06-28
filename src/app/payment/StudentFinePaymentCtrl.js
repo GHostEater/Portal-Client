0c8c969ed8122910f7aad99aacd74ed07d4c6b89
GHostEater <lasisi28@gmail.com>
2019-06-28 08:24:28 +0100

    Added Narration to Payment

--- src/app/payment/StudentFinePaymentCtrl.js
+++ src/app/payment/StudentFinePaymentCtrl.js
@@ -3,12 +3,19 @@
  * Created by P-FLEX MONEY on 26-Feb-19.
  */
 angular.module('b')
-  .controller('StudentFinePaymentCtrl', function (PaymentType,lodash) {
+  .controller('StudentFinePaymentCtrl', function (PaymentType,lodash,$state) {
     var vm = this;
-    vm.partial_pay = 0;
-    vm.custom_pay = false;
+    vm.pay = 0;
+    vm.narration = '';
+    vm.pay_fine = pay_fine;
     PaymentType.query().$promise
       .then(function (data) {
         vm.fine = lodash.find(data,{tag:'fine'});
       });
+
+    function pay_fine() {
+      if(vm.narration !== "" && vm.pay !== 0){
+        $state.go("student_pay",{payment:vm.fine.id,amount:vm.pay,narration:vm.narration});
+      }
+    }
   });
