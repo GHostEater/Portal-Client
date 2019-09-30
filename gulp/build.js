c1e5b6b62fe6571784042adebba18983f1eb162e
GHostEater <lasisi28@gmail.com>
2019-09-30 12:17:12 +0100

    CourseSlip changes and docker

--- gulp/build.js
+++ gulp/build.js
@@ -90,7 +90,7 @@ gulp.task('other', function () {
 });
 
 gulp.task('clean', function () {
-  return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
+  return $.del([path.join(conf.paths.dist, '/*'), path.join(conf.paths.tmp, '/')]);
 });
 
 gulp.task('build', ['html', 'fonts', 'other']);
